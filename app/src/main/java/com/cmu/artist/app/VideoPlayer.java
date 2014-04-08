b89df208494d8ddb262056c28896b402512e79b6
XuefengZhai <xzhai@cmu.edu>
2014-04-07 21:38:20 -0400

    Mp4 Added

--- app/src/main/java/com/cmu/artist/app/VideoPlayer.java
+++ app/src/main/java/com/cmu/artist/app/VideoPlayer.java
@@ -1,9 +1,11 @@
 package com.cmu.artist.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class VideoPlayer extends ActionBarActivity {
@@ -34,5 +36,11 @@ public class VideoPlayer extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void back(View view){
+        Intent intent = new Intent(this, InfoPage.class);
+        startActivity(intent);
+    }
+
 
 }
